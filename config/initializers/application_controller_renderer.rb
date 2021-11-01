e784263ae6281035d51863c0c8544fc4bcd1c862
takumi1213 <shinnsekai5@gmail.com>
2021-11-01 14:33:21 +0900

    new

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = '56b3c59710a56973f2bce2f4935aa0e59f21947b14b315dde4f52ca686e4ffa84cadfa2521f542cf94992527a251fa63defa28d5ecc50041621e7fdd3c8a4ec8'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  
+  devise_for :users
+  root to: "home#index"
+
+  resources :users
+  resources :recipes
+
+end
